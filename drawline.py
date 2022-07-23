561bf19709aee97a471bdd25f824f07d861c9596
Ecrincrying <1016158928@qq.com>
2022-07-23 18:37:14 +0800

    joint_SEC_01训练以及评估

--- drawline.py
+++ drawline.py
@@ -24,8 +24,8 @@ psnr_result = [
     ]
 
 
-bpp_myself = [0.11311848958333331]
-psnr_myself = [ 27.9804873104851]
+bpp_myself = [0.11311848958333331, 0.1898600260416667]
+psnr_myself = [ 27.9804873104851, 29.69223977516354]
 
 # 第一个码率点
 bpp_myself_FIR_01 = [0.11311848958333331]
@@ -33,6 +33,11 @@ psnr_myself_FIR_01 = [ 27.9804873104851]
 
 bpp_myself_FIR_02 = [0.11440022786458331]
 psnr_myself_FIR_02 = [	27.919279305731433]
+
+
+# 第二个码率点
+bpp_myself_SEC_01 = [0.1898600260416667]
+psnr_myself_SEC_01 = [29.69223977516354]
 
 # 添加横纵坐标与标题
 plt.xlabel('bit rate [bit/px]')
@@ -50,12 +55,15 @@ plt.plot(bpp_myself, psnr_myself, label='myself')
 
 
 # 第一个码率点
-plt.scatter(bpp_myself_FIR_01, psnr_myself_FIR_01)
-plt.plot(bpp_myself_FIR_01, psnr_myself_FIR_01, label='joint_FIR_01')
+# plt.scatter(bpp_myself_FIR_01, psnr_myself_FIR_01)
+# plt.plot(bpp_myself_FIR_01, psnr_myself_FIR_01, label='joint_FIR_01')
 
-plt.scatter(bpp_myself_FIR_02, psnr_myself_FIR_02)
-plt.plot(bpp_myself_FIR_02, psnr_myself_FIR_02, label='joint_FIR_02')
+# plt.scatter(bpp_myself_FIR_02, psnr_myself_FIR_02)
+# plt.plot(bpp_myself_FIR_02, psnr_myself_FIR_02, label='joint_FIR_02')
 
+# 第二个码率点
+plt.scatter(bpp_myself_SEC_01, psnr_myself_SEC_01)
+plt.plot(bpp_myself_SEC_01, psnr_myself_SEC_01, label='joint_SEC_01')
 #添加网格信息
 plt.grid(True, linestyle='--', alpha=0.5) #默认是True，风格设置为虚线，alpha为透明度
 plt.legend() # 为了能显示label
