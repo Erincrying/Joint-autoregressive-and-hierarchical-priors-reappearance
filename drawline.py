b765f61d8774eb8ce3ef54ce89f7ed983817b0ea
Ecrincrying <1016158928@qq.com>
2022-08-08 16:00:08 +0800

    joint_FIVE_01训练以及评估

--- drawline.py
+++ drawline.py
@@ -24,8 +24,8 @@ psnr_result = [
     ]
 
 
-bpp_myself = [0.11311848958333331, 0.1898600260416667, 0.34497409396701384, 0.550320095486111]
-psnr_myself = [ 27.9804873104851, 29.69223977516354, 31.70264841162641, 33.72843470073433]
+bpp_myself = [0.11311848958333331, 0.1898600260416667, 0.34497409396701384, 0.550320095486111, 0.7960645887586807]
+psnr_myself = [ 27.9804873104851, 29.69223977516354, 31.70264841162641, 33.72843470073433, 35.549931949747794]
 
 # 第一个码率点
 bpp_myself_FIR_01 = [0.11311848958333331]
@@ -53,6 +53,10 @@ psnr_myself_FOUR_01 = [33.72843470073433]
 bpp_myself_FOUR_02 = [0.5457661946614584]
 psnr_myself_FOUR_02 = [33.686422857353115]
 
+
+# 第五个码率点
+bpp_myself_FIVE_01 = [0.7960645887586807]
+psnr_myself_FIVE_01 = [35.549931949747794]
 
 # 添加横纵坐标与标题
 plt.xlabel('bit rate [bit/px]')
@@ -87,11 +91,15 @@ plt.plot(bpp_myself, psnr_myself, label='myself')
 # plt.scatter(bpp_myself_THI_02, psnr_myself_THI_02)
 # plt.plot(bpp_myself_THI_02, psnr_myself_THI_02, label='joint_THI_02')
 # 第四个码率点
-plt.scatter(bpp_myself_FOUR_01, psnr_myself_FOUR_01)
-plt.plot(bpp_myself_FOUR_01, psnr_myself_FOUR_01, label='joint_FOUR_01')
+# plt.scatter(bpp_myself_FOUR_01, psnr_myself_FOUR_01)
+# plt.plot(bpp_myself_FOUR_01, psnr_myself_FOUR_01, label='joint_FOUR_01')
 
-plt.scatter(bpp_myself_FOUR_02, psnr_myself_FOUR_02)
-plt.plot(bpp_myself_FOUR_02, psnr_myself_FOUR_02, label='joint_FOUR_02')
+# plt.scatter(bpp_myself_FOUR_02, psnr_myself_FOUR_02)
+# plt.plot(bpp_myself_FOUR_02, psnr_myself_FOUR_02, label='joint_FOUR_02')
+
+# 第五个码率点
+plt.scatter(bpp_myself_FIVE_01, psnr_myself_FIVE_01)
+plt.plot(bpp_myself_FIVE_01, psnr_myself_FIVE_01, label='joint_FIVE_01')
 
 #添加网格信息
 plt.grid(True, linestyle='--', alpha=0.5) #默认是True，风格设置为虚线，alpha为透明度
